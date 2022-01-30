24389dce41032a7755c06e72de33abe20465124b
Alexander Fedorov <alexander.fedorov@arsysop.ru>
2022-01-30 11:33:34 +0300

    Move cast.ecore to sub decl.ecore #2
    loft.cast.* -> loft.cast.decl.*
    
    Signed-off-by: Alexander Fedorov <alexander.fedorov@arsysop.ru>

---
+++ bundles/ru.arsysop.loft.cast.decl.model/src-gen/ru/arsysop/loft/cast/decl/model/util/DeclAdapterFactory.java
@@ -0,0 +1,210 @@
+/**
+ * Copyright (c) 2022 ArSysOp
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 	SPDX-License-Identifier: Apache-2.0
+ * Contributors:
+ * 	ArSysOp - initial API and implementation
+ * 
+ */
+package ru.arsysop.loft.cast.decl.model.util;
+
+import org.eclipse.emf.common.notify.Adapter;
+import org.eclipse.emf.common.notify.Notifier;
+
+import org.eclipse.emf.common.notify.impl.AdapterFactoryImpl;
+
+import org.eclipse.emf.ecore.EObject;
+
+import ru.arsysop.loft.cast.decl.model.api.*;
+import ru.arsysop.loft.cast.decl.model.meta.DeclPackage;
+
+/**
+ * <!-- begin-user-doc -->
+ * The <b>Adapter Factory</b> for the model.
+ * It provides an adapter <code>createXXX</code> method for each class of the model.
+ * <!-- end-user-doc -->
+ * @see ru.arsysop.loft.cast.decl.model.meta.DeclPackage
+ * @generated
+ */
+public class DeclAdapterFactory extends AdapterFactoryImpl {
+	/**
+	 * The cached model package.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected static DeclPackage modelPackage;
+
+	/**
+	 * Creates an instance of the adapter factory.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public DeclAdapterFactory() {
+		if (modelPackage == null) {
+			modelPackage = DeclPackage.eINSTANCE;
+		}
+	}
+
+	/**
+	 * Returns whether this factory is applicable for the type of the object.
+	 * <!-- begin-user-doc -->
+	 * This implementation returns <code>true</code> if the object is either the model's package or is an instance object of the model.
+	 * <!-- end-user-doc -->
+	 * @return whether this factory is applicable for the type of the object.
+	 * @generated
+	 */
+	@Override
+	public boolean isFactoryForType(Object object) {
+		if (object == modelPackage) {
+			return true;
+		}
+		if (object instanceof EObject) {
+			return ((EObject)object).eClass().getEPackage() == modelPackage;
+		}
+		return false;
+	}
+
+	/**
+	 * The switch that delegates to the <code>createXXX</code> methods.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected DeclSwitch<Adapter> modelSwitch =
+		new DeclSwitch<Adapter>() {
+			@Override
+			public Adapter caseNamedDecl(NamedDecl object) {
+				return createNamedDeclAdapter();
+			}
+			@Override
+			public Adapter caseTranslationUnitDecl(TranslationUnitDecl object) {
+				return createTranslationUnitDeclAdapter();
+			}
+			@Override
+			public Adapter caseNamespaceDecl(NamespaceDecl object) {
+				return createNamespaceDeclAdapter();
+			}
+			@Override
+			public Adapter caseClassTemplateDecl(ClassTemplateDecl object) {
+				return createClassTemplateDeclAdapter();
+			}
+			@Override
+			public Adapter caseFunctionDecl(FunctionDecl object) {
+				return createFunctionDeclAdapter();
+			}
+			@Override
+			public Adapter defaultCase(EObject object) {
+				return createEObjectAdapter();
+			}
+		};
+
+	/**
+	 * Creates an adapter for the <code>target</code>.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param target the object to adapt.
+	 * @return the adapter for the <code>target</code>.
+	 * @generated
+	 */
+	@Override
+	public Adapter createAdapter(Notifier target) {
+		return modelSwitch.doSwitch((EObject)target);
+	}
+
+
+	/**
+	 * Creates a new adapter for an object of class '{@link ru.arsysop.loft.cast.decl.model.api.NamedDecl <em>Named Decl</em>}'.
+	 * <!-- begin-user-doc -->
+	 * This default implementation returns null so that we can easily ignore cases;
+	 * it's useful to ignore a case when inheritance will catch all the cases anyway.
+	 * <!-- end-user-doc -->
+	 * @return the new adapter.
+	 * @see ru.arsysop.loft.cast.decl.model.api.NamedDecl
+	 * @generated
+	 */
+	public Adapter createNamedDeclAdapter() {
+		return null;
+	}
+
+	/**
+	 * Creates a new adapter for an object of class '{@link ru.arsysop.loft.cast.decl.model.api.TranslationUnitDecl <em>Translation Unit Decl</em>}'.
+	 * <!-- begin-user-doc -->
+	 * This default implementation returns null so that we can easily ignore cases;
+	 * it's useful to ignore a case when inheritance will catch all the cases anyway.
+	 * <!-- end-user-doc -->
+	 * @return the new adapter.
+	 * @see ru.arsysop.loft.cast.decl.model.api.TranslationUnitDecl
+	 * @generated
+	 */
+	public Adapter createTranslationUnitDeclAdapter() {
+		return null;
+	}
+
+	/**
+	 * Creates a new adapter for an object of class '{@link ru.arsysop.loft.cast.decl.model.api.NamespaceDecl <em>Namespace Decl</em>}'.
+	 * <!-- begin-user-doc -->
+	 * This default implementation returns null so that we can easily ignore cases;
+	 * it's useful to ignore a case when inheritance will catch all the cases anyway.
+	 * <!-- end-user-doc -->
+	 * @return the new adapter.
+	 * @see ru.arsysop.loft.cast.decl.model.api.NamespaceDecl
+	 * @generated
+	 */
+	public Adapter createNamespaceDeclAdapter() {
+		return null;
+	}
+
+	/**
+	 * Creates a new adapter for an object of class '{@link ru.arsysop.loft.cast.decl.model.api.ClassTemplateDecl <em>Class Template Decl</em>}'.
+	 * <!-- begin-user-doc -->
+	 * This default implementation returns null so that we can easily ignore cases;
+	 * it's useful to ignore a case when inheritance will catch all the cases anyway.
+	 * <!-- end-user-doc -->
+	 * @return the new adapter.
+	 * @see ru.arsysop.loft.cast.decl.model.api.ClassTemplateDecl
+	 * @generated
+	 */
+	public Adapter createClassTemplateDeclAdapter() {
+		return null;
+	}
+
+	/**
+	 * Creates a new adapter for an object of class '{@link ru.arsysop.loft.cast.decl.model.api.FunctionDecl <em>Function Decl</em>}'.
+	 * <!-- begin-user-doc -->
+	 * This default implementation returns null so that we can easily ignore cases;
+	 * it's useful to ignore a case when inheritance will catch all the cases anyway.
+	 * <!-- end-user-doc -->
+	 * @return the new adapter.
+	 * @see ru.arsysop.loft.cast.decl.model.api.FunctionDecl
+	 * @generated
+	 */
+	public Adapter createFunctionDeclAdapter() {
+		return null;
+	}
+
+	/**
+	 * Creates a new adapter for the default case.
+	 * <!-- begin-user-doc -->
+	 * This default implementation returns null.
+	 * <!-- end-user-doc -->
+	 * @return the new adapter.
+	 * @generated
+	 */
+	public Adapter createEObjectAdapter() {
+		return null;
+	}
+
+} //CastAdapterFactory
